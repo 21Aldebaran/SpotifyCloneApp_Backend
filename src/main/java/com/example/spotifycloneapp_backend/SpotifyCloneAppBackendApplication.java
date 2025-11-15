5e22380db507b7ecdc2c3f81a2d5645e5f0757ba
Santiago <santiago.cama@utec.edu.pe>
2025-11-15 00:41:17 -0500

    Initial Commit

---
+++ src/main/java/com/example/spotifycloneapp_backend/SpotifyCloneAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.spotifycloneapp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyCloneAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpotifyCloneAppBackendApplication.class, args);
+    }
+
+}
